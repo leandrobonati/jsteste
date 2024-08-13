28b349911a779092b595a37178218417d16c2ad7
Leandro Bonati <161650868+leandrobonati@users.noreply.github.com>
2024-08-13 09:20:23 -0300

    Add files via upload

---
+++ js/js/script.js
@@ -0,0 +1,7 @@
+var num1 = Number.parseFloat(window.prompt('digite o primeiro numero'))
+
+var num2 = Number.parseFloat(window.prompt('digite o segundo numero'))
+
+var resu = (num1 * num2)
+
+alert('seu resultado é '+ resu )
