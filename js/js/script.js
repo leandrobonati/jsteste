611e059b3f33e50179f73414b24df610385fb42a
Leandro Bonati <161650868+leandrobonati@users.noreply.github.com>
2024-08-13 12:37:05 -0300

    Add files via upload

--- js/js/script.js
+++ js/js/script.js
@@ -2,6 +2,14 @@ var num1 = Number.parseFloat(window.prompt('digite o primeiro numero'))
 
 var num2 = Number.parseFloat(window.prompt('digite o segundo numero'))
 
-var resu = (num1 * num2)
+var resu = (num1 + num2)
 
-alert('seu resultado é '+ resu )
+alert(`a soma de ${num1} e ${num2} é igual a ${resu}`)
+
+if (resu == 10 ) {
+    alert('parabens')
+}
+else if (resu != 10) {
+    alert('errou')
+}
+
